d8bed97b945ff5d1554338cda76039c3aa6ded33
Leonardopm27 <80799375+Leonardopm27@users.noreply.github.com>
2026-09-13 18:48:38 -0300

    Adicionando lista no app.py

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return "<h1>Criação da página </h1>"
+    return """
+    <h1>Criação da página</h1>
+    <h2>Pequena Lista<h2>
+
+    <ul>
+        <li>Estudar Python</li>
+        <li>Estudar Git</li>
+        <li>Aprender Docker</li>
+    </ul>
+    """
+
 
 if __name__ == "__main__":
     app.run(debug=True)
